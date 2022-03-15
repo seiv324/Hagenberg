bdca707ab09d39d17b80c279aeb1a7583fa38374
Stefan <stefan.habringer@gmail.com>
2022-03-15 20:45:22 +0100

    Added Data Analysis in python hurray ;D

--- SS_2022/SWE/U_02/skipList/skipList/main.cpp
+++ SS_2022/SWE/U_02/skipList/skipList/main.cpp
@@ -14,6 +14,7 @@ int main() {
 
   ////for (int i = 0; i < 100; i++) {
   ////  cout << s.gen_rand_level(32) << endl;
+  //    s.insert(gen_rand_level(32));
   ////}
   ////cout << s << endl;
   // 
@@ -28,18 +29,21 @@ int main() {
   // Start Runtime Evaluation:
   // Run each Funktion with the provided number of elements
   // at least 25/15/10 times (depending on the number of elements)
-  int counter = 8000;
-  while(counter <= 10000){ // Pro Einheit 25 Werte
-    for (int i = 0; i < 15; i++) {
-      simulate_own_set_int(counter);
-      simulate_own_set_string(counter);
-      simulate_set_int(counter);
-      simulate_set_string(counter);
-    }
-    cout << "Counter " << counter << endl;
-    counter += 1000;
-  };
+  //int counter = 10;
+  //while(counter <= 10){ // Pro Einheit 25 Werte
+  //  for (int i = 0; i < 3; i++) {
+  //    simulate_own_set_int(counter);
+  //    simulate_own_set_string(counter);
+  //    simulate_set_int(counter);
+  //    simulate_set_string(counter);
+  //  }
+  //  cout << "Counter " << counter << endl;
+  //  counter += 1000;
+  //};
 
+  // Stopped at 55 000 iterations -> writing is to slow
+
+  simulate_all(100000);
 
   return 0;
 }
--- SS_2022/SWE/U_02/skipList/skipList/simulation_file.h
+++ SS_2022/SWE/U_02/skipList/skipList/simulation_file.h
@@ -77,9 +77,12 @@ string generate_rand_string(int len) {
     "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     "abcdefghijklmnopqrstuvwxyz";
   std::string tmp_s;
-  tmp_s.reserve(len);
-
-  for (int i = 0; i < len; ++i) {
+  int tmp_len = len;
+  if (len > 150) {
+    tmp_len = 150;
+  }
+  tmp_s.reserve(tmp_len);
+  for (int i = 0; i < tmp_len; i++) {
     tmp_s += alphanum[rand() % (sizeof(alphanum) - 1)];
   }
 
@@ -184,47 +187,146 @@ void simulate_set_int(int n) {
 /// Simulate Insert/Find/Erase of n random strings STL set implementation
 /// </summary>
 /// <param name="n"> number of random strings </param>
-void simulate_set_string(int n) {
+void simulate_all(int n) {
+  std::ofstream ofile1;
+  std::ofstream ofile2;
+  std::ofstream ofile3;
+  std::ofstream ofile4;
 
-  /*cout << "----- Start Run with n ( " << n << " ) random strings in STL set -----" << endl;*/
-  set<string> s;
+  int counter = 50000;
+  int counter_raise = 10000;
 
-  auto start = high_resolution_clock::now();
-  for (int i = 0; i < n; i++) {
-    s.insert(generate_rand_string(rand()));
-  }
-  auto end = high_resolution_clock::now();
-  duration<double> diff_ins = end - start;
- 
-  start = high_resolution_clock::now();
-  for (int i = 0; i < n; i++) {
-    s.find(generate_rand_string(rand()));
-  }
-  end = high_resolution_clock::now();
-  duration<double> diff_find = end - start;
+  ofile1.open("results_int_self.txt", std::ios::app);
+  ofile2.open("results_int_set.txt", std::ios::app);
+  ofile3.open("results_string_self.txt", std::ios::app);
+  ofile4.open("results_string_set.txt", std::ios::app);
 
-  start = high_resolution_clock::now();
-  for (int i = 0; i < n; i++) {
-    s.erase(generate_rand_string(rand()));
-  }
-  end = high_resolution_clock::now();
-  duration<double> diff_erase = end - start;
-
-  //cout << "Time Insert: " << (diff_ins).count() << endl;
-  //cout << "Time Search: " << (diff_find).count() << endl;
-  //cout << "Time Erase: " << (diff_erase).count() << endl;
-  //cout << "----- End Run with n ( " << n << " ) random STL in skip set -----" << endl;
-  //cout << endl;
-
-  std::ofstream ofile;
   std::setprecision(6);
-  ofile.open("results_string_set.txt", std::ios::app); //app is append which means it will put the text at the end
 
-  ofile << "ins;" << n << ";" << (diff_ins).count() << endl;
-  ofile << "find;" << n << ";" << (diff_find).count() << endl;
-  ofile << "erase;" << n << ";" << (diff_erase).count() << endl;
+  while(counter < n){
+    // OWN INT
+    Skip_set<int> s1(0, 0.5);
+    for (int i = 0; i < 20; i++) {
+      auto start = high_resolution_clock::now();
+      for (int i = 0; i < counter; i++) {
+        s1.insert(rand());
+      }
+      auto end = high_resolution_clock::now();
+      duration<double> diff_ins = end - start;
+      start = high_resolution_clock::now();
+      for (int i = 0; i < counter; i++) {
+        s1.find(rand());
+      }
+      end = high_resolution_clock::now();
+      duration<double> diff_find = end - start;
+      start = high_resolution_clock::now();
+      for (int i = 0; i < counter; i++) {
+        s1.erase(rand());
+      }
+      end = high_resolution_clock::now();
+      duration<double> diff_erase = end - start;
+      ofile1 << "ins;" << counter << ";" << (diff_ins).count() << endl;
+      ofile1 << "find;" << counter << ";" << (diff_find).count() << endl;
+      ofile1 << "erase;" << counter << ";" << (diff_erase).count() << endl;
+    }
+    //cout << "Int Self | ";
+    // SET INT
+    
+    set<int> s2;
+    for (int i = 0; i < 20; i++) {
+      auto start = high_resolution_clock::now();
+      for (int i = 0; i < counter; i++) {
+        s2.insert(rand());
+      }
+      auto end = high_resolution_clock::now();
+      duration<double> diff_ins = end - start;
+      start = high_resolution_clock::now();
+      for (int i = 0; i < counter; i++) {
+        s2.find(rand());
+      }
+      end = high_resolution_clock::now();
+      duration<double> diff_find = end - start;
+      start = high_resolution_clock::now();
+      for (int i = 0; i < counter; i++) {
+        s2.erase(rand());
+      }
+      end = high_resolution_clock::now();
+      duration<double> diff_erase = end - start;
+      ofile2 << "ins;" << counter << ";" << (diff_ins).count() << endl;
+      ofile2 << "find;" << counter << ";" << (diff_find).count() << endl;
+      ofile2 << "erase;" << counter << ";" << (diff_erase).count() << endl;
+    }
+    //cout << "Int Set | ";
 
-  ofile.close();
+    // OWN STRING
+    Skip_set<string> s3("", 0.5);
+    for (int i = 0; i < 20; i++) {
+      auto start = high_resolution_clock::now();
+      for (int i = 0; i < counter; i++) {
+        s3.insert(generate_rand_string(rand()));
+      }
+      auto end = high_resolution_clock::now();
+      duration<double> diff_ins = end - start;
+
+      start = high_resolution_clock::now();
+      for (int i = 0; i < counter; i++) {
+        s3.find(generate_rand_string(rand()));
+      }
+      end = high_resolution_clock::now();
+      duration<double> diff_find = end - start;
+
+      start = high_resolution_clock::now();
+      for (int i = 0; i < counter; i++) {
+        s3.erase(generate_rand_string(rand()));
+      }
+      end = high_resolution_clock::now();
+      duration<double> diff_erase = end - start;
+
+      ofile3 << "ins;" << counter << ";" << (diff_ins).count() << endl;
+      ofile3 << "find;" << counter << ";" << (diff_find).count() << endl;
+      ofile3 << "erase;" << counter << ";" << (diff_erase).count() << endl;
+    }
+    //cout << "String Self | ";
+
+    // STL String
+    set<string> s4;
+    
+    for (int i = 0; i < 20; i++) {
+      auto start = high_resolution_clock::now();
+      for (int i = 0; i < counter; i++) {
+        s4.insert(generate_rand_string(rand()));
+      }
+      auto end = high_resolution_clock::now();
+      duration<double> diff_ins = end - start;
+
+      start = high_resolution_clock::now();
+      for (int i = 0; i < counter; i++) {
+        s4.find(generate_rand_string(rand()));
+      }
+      end = high_resolution_clock::now();
+      duration<double> diff_find = end - start;
+
+      start = high_resolution_clock::now();
+      for (int i = 0; i < counter; i++) {
+        s4.erase(generate_rand_string(rand()));
+      }
+      end = high_resolution_clock::now();
+      duration<double> diff_erase = end - start;
+
+      ofile4 << "ins;" << counter << ";" << (diff_ins).count() << endl;
+      ofile4 << "find;" << counter << ";" << (diff_find).count() << endl;
+      ofile4 << "erase;" << counter << ";" << (diff_erase).count() << endl;
+    }
+    //cout << "String Set | ";
+
+    counter += counter_raise;
+    cout << counter <<endl;
+  }
+    ofile1.close();
+    ofile2.close();
+    ofile3.close();
+    ofile4.close();
+  
 }
 
 #endif // !SIMULATIONFILE_H
